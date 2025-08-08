87511a01058a48777e3e91171355e793ff951848
Admin <Admin@ELLA>
2025-08-09 06:07:25 +0800

    Add project files.

---
+++ activity 15/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> courses = new List<string>();
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write("Enter course code: ");
+            string code = Console.ReadLine();
+            if (code.Length != 5)
+            {
+                Console.WriteLine("Error: Course code must be 5 characters.");
+                continue;
+            }
+            if (courses.Contains(code))
+            {
+                Console.WriteLine("Error: Duplicate course code.");
+            }
+            else
+            {
+                courses.Add(code);
+            }
+        }
+    }
+}
